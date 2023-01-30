a22d8db129d091905fa8f16ba03f9b568cabbe70
Black_Shadow <104593332+Utkboi69@users.noreply.github.com>
2023-01-30 19:42:53 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.random();
+randomNumber1 = Math.floor(randomNumber1*6) + 1;
+
+
+var randomImage = 'dice' + randomNumber1 + '.png';
+var randomImageSource = 'images/' + randomImage;
+
+img1 = document.querySelectorAll('img')[0];
+img1.setAttribute('src',randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomImageSource2 = 'images/dice' + randomNumber2 + '.png';
+
+img2 = document.querySelectorAll('img')[1];
+img2.setAttribute('src',randomImageSource2);
+
+if(randomNumber1>randomNumber2)
+document.querySelector('h1').innerHTML = 'Player 1 wins! 🎉';
+else if(randomNumber1 === randomNumber2)
+document.querySelector('h1').innerHTML = 'Its a Draw! 😑';
+else
+document.querySelector('h1').innerHTML = 'Player 2 wins! 🎉';
